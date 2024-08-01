f741e2f6add08d21543efa8c8a14e6029388495f
davidfer1112 <davidfernando1112@gmail.com>
2024-07-31 22:27:46 -0500

    First commit

---
+++ src/main/java/com/example/demo/entities/GetCountryResponse.java
@@ -0,0 +1,71 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.07.31 a las 10:18:25 PM COT 
+//
+
+
+package com.example.demo.entities;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="country" type="{http://www.example.com/demo/entities}country"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "country"
+})
+@XmlRootElement(name = "getCountryResponse")
+public class GetCountryResponse {
+
+    @XmlElement(required = true)
+    protected Country country;
+
+    /**
+     * Obtiene el valor de la propiedad country.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Country }
+     *     
+     */
+    public Country getCountry() {
+        return country;
+    }
+
+    /**
+     * Define el valor de la propiedad country.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Country }
+     *     
+     */
+    public void setCountry(Country value) {
+        this.country = value;
+    }
+
+}
